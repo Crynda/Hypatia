4d9dfcf4a981ea6a9b9fbdf9131b9895865af464
LENOVO <LENOVO@DESKTOP-P1UDURL>
2026-06-30 20:41:44 -0600

    Se agrego un mensaje de confirmacion al realizar cambios en la configuracion

--- src/gui/controladores/ConfigController.java
+++ src/gui/controladores/ConfigController.java
@@ -2,16 +2,26 @@ package gui.controladores;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import gui.config.Configuracion;
 import gui.config.GestorConfiguracion;
 import gui.config.GestorTemas;
 import gui.util.SwitchToggle;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
 
 public class ConfigController {
 
@@ -35,9 +45,74 @@ public class ConfigController {
 
     @FXML
     private Configuracion config;
-
+    
+    @FXML
     private TableController tableController;
+    
+ // Animaciones activas
+    private FadeTransition fadeEntrada;
+    private FadeTransition fadeSalida;
+    private Timeline tiempoBarra;
+    
+    // Notificaciones
+    @FXML
+    private Pane pnlNotificacion;
 
+    @FXML
+    private Label lblMensaje;
+    
+    @FXML
+    private Rectangle rectTiempo;
+
+    @FXML
+    public void mostrarNotificacion() {
+
+        // Detener cualquier animación anterior
+        if (fadeEntrada != null) fadeEntrada.stop();
+        if (fadeSalida != null) fadeSalida.stop();
+        if (tiempoBarra != null) tiempoBarra.stop();
+
+
+        // Reiniciar estado
+        lblMensaje.setTextFill(Color.web("#003EFF"));
+        pnlNotificacion.setVisible(true);
+        pnlNotificacion.setOpacity(0);
+
+        rectTiempo.setWidth(600);
+
+        // Fade de entrada
+        fadeEntrada = new FadeTransition(Duration.millis(175), pnlNotificacion);
+        fadeEntrada.setFromValue(0);
+        fadeEntrada.setToValue(1);
+
+        // Barra de tiempo (3 segundos)
+        tiempoBarra = new Timeline(
+            new KeyFrame(
+                Duration.seconds(1.3),
+                new KeyValue(rectTiempo.widthProperty(), 0)
+            )
+        );
+
+        // Al terminar la barra, hacer fade de salida
+        tiempoBarra.setOnFinished(e -> {
+
+            fadeSalida = new FadeTransition(Duration.millis(175), pnlNotificacion);
+            fadeSalida.setFromValue(1);
+            fadeSalida.setToValue(0);
+
+            fadeSalida.setOnFinished(ev -> {
+                pnlNotificacion.setVisible(false);
+            });
+
+            fadeSalida.play();
+
+        });
+
+        fadeEntrada.play();
+        tiempoBarra.play();
+    }
+    
+    
     // =========================
     // INIT
     // =========================
@@ -105,6 +180,8 @@ public class ConfigController {
         if (tableController != null) {
             tableController.setLimiteDecimales(valor);
         }
+        
+        mostrarNotificacion();
 
     }
     
